4cac109b3a36e07a86faec51c99aec7cd84b291c
hehehai <riverhohai@gmail.com>
2024-03-27 14:16:51 +0800

    chore: db config

--- src/env.js
+++ src/env.js
@@ -4,6 +4,7 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
+    DIRECT_URL: z.string().url(),
     NODE_ENV: z.enum(["development", "production"]).default("development"),
     NEXTAUTH_SECRET:
       process.env.NODE_ENV === "production"
@@ -33,6 +34,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    DIRECT_URL: process.env.DIRECT_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
