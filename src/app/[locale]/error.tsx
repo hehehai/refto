622063e5dbd8cc985c957ec456a53960350ea107
hehehai <riverhohai@gmail.com>
2024-04-27 12:37:59 +0800

    chore: i18n error & 404

--- src/app/[locale]/error.tsx
+++ src/app/[locale]/error.tsx
@@ -5,14 +5,17 @@ import { TerminalIcon } from "lucide-react";
 
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
+import { getTranslations } from "next-intl/server";
 
-export default function Error({
+export default async function Error({
   error,
   reset,
 }: {
   error: Error;
   reset: () => void;
 }) {
+  const t = await getTranslations("Error");
+
   return (
     <div className="flex h-[80vh] w-full flex-col items-center justify-center">
       <Alert className="my-6 w-[40%]">
@@ -29,11 +32,11 @@ export default function Error({
             }&body=${error.message}`}
             target="_blank"
           >
-            Make an Issue
+            {t("fallback")}
           </Link>
         </Button>
         <Button onClick={() => reset()} className="ml-3">
-          Try again
+          {t("button")}
         </Button>
       </div>
     </div>
--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,21 +1,23 @@
 import { Button } from "@/components/ui/button";
+import { getTranslations } from "next-intl/server";
 import Link from "next/link";
 import React from "react";
 
-function notFound() {
+async function notFound() {
+  const t = await getTranslations("NotFound");
   return (
     <main className="grid min-h-screen place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
         <p className="text-base font-semibold text-gray-900">404</p>
         <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          Page not found
+          {t("title")}
         </h1>
         <p className="mt-6 text-base leading-7 text-gray-600">
-          Sorry! We can't find the page you're looking for.
+          {t("description")}
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <Button asChild>
-            <Link href="/">Back to home</Link>
+            <Link href="/">{t("back")}</Link>
           </Button>
         </div>
       </div>
