ed2255086ab872cca3be1efb0d4ee596dd793fe0
hehehai <riverhohai@gmail.com>
2025-11-23 18:04:31 +0800

    fix: remove placeholderChar from DOM props in InputOTPSlot

--- src/components/ui/input-otp.tsx
+++ src/components/ui/input-otp.tsx
@@ -29,24 +29,29 @@ InputOTPGroup.displayName = "InputOTPGroup";
 const InputOTPSlot = React.forwardRef<
   React.ElementRef<"div">,
   SlotProps & React.ComponentPropsWithoutRef<"div">
->(({ char, hasFakeCaret, isActive, className, ...props }, ref) => (
-  <div
-    className={cn(
-      "relative flex h-14 w-14 items-center justify-center border-input border-y border-r text-3xl shadow-xs transition-all first:rounded-l-md first:border-l last:rounded-r-md",
-      isActive && "z-10 ring-1 ring-ring",
-      className
-    )}
-    ref={ref}
-    {...props}
-  >
-    {char}
-    {hasFakeCaret && (
-      <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
-        <div className="h-4 w-px animate-caret-blink bg-foreground duration-1000" />
-      </div>
-    )}
-  </div>
-));
+>(
+  (
+    { char, hasFakeCaret, isActive, placeholderChar, className, ...props },
+    ref
+  ) => (
+    <div
+      className={cn(
+        "relative flex h-14 w-14 items-center justify-center border-input border-y border-r text-3xl shadow-xs transition-all first:rounded-l-md first:border-l last:rounded-r-md",
+        isActive && "z-10 ring-1 ring-ring",
+        className
+      )}
+      ref={ref}
+      {...props}
+    >
+      {char}
+      {hasFakeCaret && (
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
+          <div className="h-4 w-px animate-caret-blink bg-foreground duration-1000" />
+        </div>
+      )}
+    </div>
+  )
+);
 InputOTPSlot.displayName = "InputOTPSlot";
 
 const InputOTPSeparator = React.forwardRef<
